b45024c607cfe1866473157da6d220dfce0b949e
Chema <chema_013@hotmail.com>
2020-08-03 20:23:09 -0500

    first commit

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,23 @@
+//comando para stablecer la conexion
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connection', function() {
+    console.log('Usuario conectado');
+});
+
+socket.on('disconnect', function() {
+    console.log('Usuario desconectado');
+});
+
+socket.on('estadoActual', function(resp) {
+    console.log(resp);
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
